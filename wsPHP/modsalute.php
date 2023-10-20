2e14f16ad025942b445fe5339099781450e74f07
lorped <lorped@gmail.com>
2023-10-20 22:14:28 +0200

    modsalute

--- wsPHP/modsalute.php
+++ wsPHP/modsalute.php
@@ -18,20 +18,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
  
 include ('db2.inc.php');  // MYSQLI //
 include ('token.php');
-include ('dado.inc.php');
-include ('dicepool.inc.php');
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 
 $token=$request->token;
-
-$IDattributo=$request->IDattributo;
-$IDskill=$request->IDskill;
-$difficolta=$request->difficolta;
-
-$stanza = $request -> stanza;
-
 
 
 if ( CheckJWT ($token) ) {
@@ -57,71 +48,16 @@ $stanza = 23 ;
 */
 
 
-$dp = 0;
 
 
 
-
-$dp = dicepool($Userid, $IDattributo, $IDskill, '');
-
-
-$MySql = "SELECT ModSalute FROM Personaggio 
+$MySql = "SELECT ModSalute, DescSalute Desc FROM Personaggio 
 	LEFT join Livelli_salute on Personaggio.IDsalute = Livelli_salute.IDsalute
 	WHERE Userid = $Userid";
 $Result = mysqli_query($db, $MySql);
 $res = mysqli_fetch_array($Result);
 
-$dp = $dp + $res['ModSalute'];
-
-if ( $dp < 0 ) {
-	$dp = 1 ;
-}
-
-
-
-if ( $IDattributo == 2) {  //DESTREZZA
-    $MySql="SELECT LivelloDisc FROM Discipline WHERE IDdisciplina = 16 AND Userid =$Userid"; 
-    $Request=mysqli_query($db, $MySql);
-    $res=mysqli_fetch_array($Request);
-    $vel = $res['LivelloDisc']; 
-    $dp = $dp + $vel;
-}
-
-
-
-$esito = $NomeCognome . " effettua un check su ";
-
-$MySql= "SELECT * FROM Attributi_main WHERE IDattributo = $IDattributo ";
-$Result = mysqli_query($db, $MySql);
-$res=mysqli_fetch_array($Result);
-
-$esito = $esito . $res['NomeAttributo'];
-
-
-
-if ( $IDskill != 0) {
-    $MySql= "SELECT * FROM Skill_main WHERE IDskill = $IDskill ";
-    $Result = mysqli_query($db, $MySql);
-    $res=mysqli_fetch_array($Result);
-    $esito = $esito . " e " . $res['NomeSkill'];
-}
-
-$esito = $esito . " a difficoltà ". $difficolta;
-
-
-
-$ee = dado($dp, $difficolta, false);
-$esito = $esito .  $ee['esito'];
-
-
-
-$MySql="INSERT INTO Chat ( Stanza, IDMittente, Mittente, IDDestinatario, Destinatario, Sesso , Tipo, Testo, Locazione )
-VALUES ($stanza, $Userid, '$NomeCognome' , 0, '' , '$Sesso', '+', '$esito', '' )";
-mysqli_query($db, $MySql);
-
-$out = [
-    'esito' => $esito
-    ];
+$out = $res ;
 
 header("HTTP/1.1 200 OK");
 
